349707414a9bf7b6097aee8651bb99711a8ac710
Vladimir Lagunov <lagunov@yandex-team.ru>
2017-01-05 23:00:24 +0700

    new theme

--- lisp/config_my_powerline.el
+++ lisp/config_my_powerline.el
@@ -54,9 +54,6 @@
     result))
 (advice-add 'airline-get-vc :around '-airline-get-vc--around)
 
-
-(require 'airline-lagunov-theme)
-
 (defun my-flycheck-mode-line-status-text (&optional status)
   (let ((param-tuple
          (pcase (or status flycheck-last-status-change)
@@ -75,11 +72,153 @@
         (put-text-property 0 (length text)'face face text))
       text)))
 
+
+(setq-default
+ mode-line-format
+ '("%e"
+   (:eval
+    (let* ((active (powerline-selected-window-active))
+           (width (window-total-width))
+           (expance (< 100 width))
+           (tight (> 70 width))
+           (separator-left (intern (format "powerline-%s-%s"
+                                           (powerline-current-separator)
+                                           (car powerline-default-separator-dir))))
+           (separator-right (intern (format "powerline-%s-%s"
+                                            (powerline-current-separator)
+                                            (cdr powerline-default-separator-dir))))
+           (mode-line-face (if active 'mode-line 'mode-line-inactive))
+
+           (outer-face
+            (if (powerline-selected-window-active) 'airline-normal-outer 'powerline-inactive1))
+
+           (inner-face
+            (if (powerline-selected-window-active) 'airline-normal-inner 'powerline-inactive2))
+
+           (center-face
+            (if (powerline-selected-window-active) 'airline-normal-center 'powerline-inactive2))
+
+           ;; Left Hand Side
+           (lhs-mode
+            (list
+             ;; Mule Info
+             (when powerline-display-mule-info
+               (powerline-raw mode-line-mule-info outer-face 'l))
+
+             ;; Modified string
+             (powerline-raw "%*" outer-face 'l)
+             ;; Separator >
+             (when expance (powerline-raw " " outer-face))
+             (funcall separator-left outer-face inner-face)))
+
+           (lhs-rest (list
+                      ;; ;; Separator >
+                      ;; (powerline-raw (char-to-string #x2b81) inner-face 'l)
+
+                      ;; Eyebrowse current tab/window config
+                      (if (featurep 'eyebrowse)
+                          (powerline-raw (concat " " (eyebrowse-mode-line-indicator)) inner-face))
+
+                      ;; LN character, Current Line and % location in file
+                      (powerline-raw
+                       (concat
+                        (if expance (char-to-string airline-utf-glyph-linenumber) "")
+                        "%l:%c")
+                       inner-face 'l)
+
+                      ;; % location in file
+                      (when expance (powerline-raw "(%p)" inner-face 'r))
+
+                      ;; Buffer Size
+                      (when (and expance powerline-display-buffer-size)
+                        (powerline-buffer-size inner-face 'l))
+
+                      ;; Separator >
+                      (when expance (powerline-raw " " inner-face))
+                      (funcall separator-left inner-face center-face)
+                      (when expance (powerline-raw " " center-face))
+
+                      ;; Directory
+                      ;; (when (eq airline-display-directory 'airline-directory-shortened)
+                      ;;   (powerline-raw (airline-shorten-directory default-directory airline-shortened-directory-length) center-face 'l))
+                      ;; (when (eq airline-display-directory 'airline-directory-full)
+                      ;;   (powerline-raw default-directory center-face 'l))
+                      ;; (when (eq airline-display-directory nil)
+                      ;;   (powerline-raw " " center-face))
+
+                      ;; Buffer ID
+                      ;; (powerline-buffer-id center-face)
+                      (powerline-raw "%b" center-face)
+
+                      ;; Narrow if appropriate
+                      (powerline-raw "%n" center-face)
+
+                      ;; ;; Separator >
+                      ;; (powerline-raw " " center-face)
+                      ;; (funcall separator-left mode-line face1)
+
+                      (when (boundp 'erc-modified-channels-object)
+                        (powerline-raw erc-modified-channels-object center-face 'l))
+
+                      ;; ;; Separator <
+                      ;; (powerline-raw " " face1)
+                      ;; (funcall separator-right face1 face2)
+                      ))
+
+           (lhs (append lhs-mode lhs-rest))
+
+           ;; Right Hand Side
+           (rhs (if tight
+                    ;; Separator <
+                    (list (powerline-raw "✂" center-face)
+                          (funcall separator-right center-face outer-face))
+
+                  (list (powerline-raw global-mode-string center-face 'r)
+
+                        ;; Separator <
+                        (when expance (powerline-raw " " center-face))
+                        (funcall separator-right center-face inner-face)
+
+                        ;; Major Mode
+                        (powerline-major-mode inner-face 'l)
+                        (powerline-process inner-face)
+
+                        ;; Subseparator <
+                        (powerline-raw (char-to-string airline-utf-glyph-subseparator-right) inner-face 'l)
+
+                        ;; Minor Modes
+                        (powerline-minor-modes inner-face 'l)
+                        ;; (powerline-narrow center-face 'l)
+
+                        (powerline-raw " " inner-face)
+
+                        ;; Separator <
+                        (funcall separator-right inner-face outer-face)
+
+                        ;; Projectile
+                        (when (not (file-remote-p default-directory))
+                          (powerline-raw (concat "⊆" (projectile-project-name)) outer-face))
+
+                        ;; Git Branch
+                        (when expance (powerline-raw (airline-get-vc) outer-face))
+
+                        (powerline-raw " " outer-face)
+                        ))
+                ))
+
+      ;; Combine Left and Right Hand Sides
+      (concat (powerline-render lhs)
+              (powerline-fill center-face (powerline-width rhs))
+              (powerline-render rhs))))))
+
+
+(require 'mytheme2017)
+;; (load-theme 'mytheme2017)
+(powerline-reset)
+
+
 (dim-minor-names
- '((projectile-mode (:eval
-                     (if (file-remote-p default-directory)
-                         ""
-                       (format " ⊆%s" (projectile-project-name)))))
+ '((projectile-mode "")
    (auto-revert-mode "")
    (flycheck-mode (:eval (my-flycheck-mode-line-status-text)))
    (helm-mode " ♚")
---
+++ lisp/mytheme2017.el
@@ -0,0 +1,63 @@
+;;; https://emacsfodder.github.io/emacs-theme-editor/ + напильник
+
+
+(deftheme mytheme2017 "DOCSTRING for mytheme2017")
+(custom-theme-set-faces
+ 'mytheme2017
+ '(default ((t (:foreground "#000000" :background "#ffffff" ))))
+ '(cursor ((t (:background "#000000" ))))
+ '(fringe ((t (:background "#f4f4f4" ))))
+ '(mode-line ((t (:foreground "#000000" :background "#d0d0d0" ))))
+ '(region ((t (:background "#ffdcb5" ))))
+ '(secondary-selection ((t (:background "#e3e3e3" ))))
+ '(font-lock-builtin-face ((t (:foreground "#3500a5" ))))
+ '(font-lock-comment-face ((t (:foreground "#71788e" :background "#fbfcff"))))
+ '(font-lock-function-name-face ((t (:foreground "#d46700" :background "#fffcfa" :weight regular))))
+ '(font-lock-keyword-face ((t (:foreground "#00b5c7" ))))
+ '(font-lock-string-face ((t (:foreground "#aeaa00" ))))
+ '(font-lock-type-face ((t (:foreground "#71009d" :background "#fcfaff"))))
+ '(font-lock-constant-face ((t (:foreground "#65009f" ))))
+ '(font-lock-variable-name-face ((t (:foreground "#d4a800" :background "#fffcf6"))))
+ '(minibuffer-prompt ((t (:foreground "#000000" :bold t ))))
+ '(font-lock-warning-face ((t (:foreground "red" :bold t )))))
+
+
+(require 'use-package)
+(use-package airline-themes)
+(let ((normal-outer-foreground  "#ffffff") (normal-outer-background  "#225dab")
+      (normal-inner-foreground  "#ffffff") (normal-inner-background  "#008894")
+      (normal-center-foreground "#000000") (normal-center-background "#d3f7a3")
+
+      (inactive1-foreground  "#f0f0f0") (inactive1-background  "#6885ab")
+      (inactive2-foreground  "#404040") (inactive2-background  "#bdcfd0")
+      (inactive3-foreground "#202020") (inactive3-background "#c4d4ae")
+
+      ;; wtf? похоже, используется только в eshell и evil
+      (insert-outer-foreground  "#ffffff") (insert-outer-background  "#225dab")
+      (insert-inner-foreground  "#ffffff") (insert-inner-background  "#008894")
+      (insert-center-foreground "#000000") (insert-center-background "#d3f7a3")
+
+      ;; используется только в evil, но нельзя не указать
+      (visual-outer-foreground  "#ffffff") (visual-outer-background  "#484a4e")
+      (visual-inner-foreground  "#c5c8c6") (visual-inner-background  "#373b41")
+      (visual-center-foreground "#000000") (visual-center-background "#de935f")
+      (replace-outer-foreground "#000000") (replace-outer-background "#cc6666")
+      (replace-inner-foreground "#00ff00") (replace-inner-background "#0000ff")
+      (replace-center-foreground "#aaff00") (replace-center-background "#cc00ff")
+      (emacs-outer-foreground   "#d7d7ff") (emacs-outer-background   "#5f00af")
+      (emacs-inner-foreground "#00ffaa") (emacs-inner-background "#00aaff")
+      (emacs-center-foreground "#aaffaa") (emacs-center-background "#ccaaff"))
+
+  (airline-themes-set-deftheme 'mytheme2017))
+
+
+;;;###autoload
+(and load-file-name
+    (boundp 'custom-theme-load-path)
+    (add-to-list 'custom-theme-load-path
+                 (file-name-as-directory
+                  (file-name-directory load-file-name))))
+;; Automatically add this theme to the load path
+
+(provide-theme 'mytheme2017)
+(provide 'mytheme2017) ;; TODO: неправильно
